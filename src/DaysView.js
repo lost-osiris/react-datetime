50b5886ae3a6a8e09909e2c46ed40f545b160f6f
Matthew Owens <mowens@redhat.com>
2016-10-13 18:29:50 -0400

    reverted on click back and just removed text in span

--- src/DaysView.js
+++ src/DaysView.js
@@ -17,9 +17,9 @@ var DateTimePickerDays = React.createClass({
 		tableChildren = [
 			DOM.thead({ key: 'th'}, [
 				DOM.tr({ key: 'h'}, [
-					DOM.th({ key: 'p', className: 'rdtPrev', onClick: this.props.subtractTime(1, 'months')}, DOM.span({}, '‹')),
+					DOM.th({ key: 'p', className: 'rdtPrev'}, DOM.span({onClick: this.props.subtractTime(1, 'months')}, '')),
 					DOM.th({ key: 's', className: 'rdtSwitch', onClick: this.props.showView('months'), colSpan: 5, 'data-value': this.props.viewDate.month() }, locale.months( date ) + ' ' + date.year() ),
-					DOM.th({ key: 'n', className: 'rdtNext', onClick: this.props.subtractTime(1, 'months') }, DOM.span({}, '›'))
+					DOM.th({ key: 'n', className: 'rdtNext',  }, DOM.span({onClick: this.props.subtractTime(1, 'months')}, ''))
 				]),
 				DOM.tr({ key: 'd'}, this.getDaysOfWeek( locale ).map( function( day, index ){ return DOM.th({ key: day + index, className: 'dow'}, day ); }) )
 			]),
